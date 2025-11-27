aba7983e3b16c07556756914ee7757bc94266f66
Onda02 <ondeialbertoinfo@gmail.com>
2025-11-27 18:02:02 +0000

    Starting point

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,22 @@
-#include <deal.II/base/convergence_table.h>
-
-#include <fstream>
-#include <iostream>
-#include <vector>
-
-#include "Poisson3D.hpp"
+#include "Wave.hpp"
 
 // Main function.
 int
 main(int argc, char *argv[])
 {
-
   Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
 
-  const std::string  mesh_filename = "../mesh/mesh-cube-40.msh";
-  constexpr unsigned int degree        = 1;
+  const std::string  mesh_file_name = "../mesh/mesh-cube-20.msh";
+  const unsigned int degree         = 1;
 
-  Poisson3D problem(mesh_filename, degree);
+  const double T      = 1.0;
+  const double deltat = 0.05;
+  const double theta  = 1.0;
+
+  Wave problem(mesh_file_name, degree, T, deltat, theta);
 
   problem.setup();
-  problem.assemble();
   problem.solve();
-  problem.output();
+
   return 0;
 }
