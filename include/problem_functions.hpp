96af697bf336964b3a86923a72b4bb76bcfb3764
AndreVale69 <sforzini31@gmail.com>
2025-12-18 01:39:34 +0100

    feat(Boundary): add homogeneous Dirichlet boundary condition class
    Refs: #3

---
+++ include/problem_functions.hpp
@@ -0,0 +1,20 @@
+#ifndef NM4PDE_PROBLEM_FUNCTIONS_HPP
+#define NM4PDE_PROBLEM_FUNCTIONS_HPP
+
+#include <deal.II/base/function.h>
+
+using namespace dealii;
+
+/**
+ * @brief Homogeneous Dirichlet boundary condition g = 0.
+ * @tparam dim Spatial dimension.
+ */
+template<int dim>
+class BoundaryGZero : public Function<dim> {
+public:
+    BoundaryGZero() : Function<dim>(1) {}
+
+    double value(const Point<dim> &, const unsigned int = 0) const override { return 0.0; }
+};
+
+#endif // NM4PDE_PROBLEM_FUNCTIONS_HPP
